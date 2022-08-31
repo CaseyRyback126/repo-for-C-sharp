007eaeb13b574fc15532a1d5cf62829c9b291d62
Casey_Ryback <serega126@list.ru>
2022-09-01 00:51:00 +0300

    Example010

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 21, 38, 84, 45, 82, 64, 76, 82 };
+
+int n = array.Length;
+int find = 82;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
