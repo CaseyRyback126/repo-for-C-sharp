2d76d5fb679658abf0325ed7ca643f2b5e3abc73
Casey_Ryback <serega126@list.ru>
2022-09-14 23:02:28 +0300

    Ex002andEx012

--- Examples012_Methods/Program.cs
+++ Examples012_Methods/Program.cs
@@ -1,40 +1,37 @@
 ﻿// Вид 1
-void Method1()
-{
-    Console.WriteLine("Автор ...");
-}
+// void Method1()
+// {
+//     Console.WriteLine("Автор ...");
+// }
 //Method1();
 
 
-
-
 //Вид 2
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
 //Method2("Текст сообщения");
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
 //Method21("Текст",4);
 //Method21(count:4, msg: "новый текст");
 
 
-
 //Вид 3
 
-int Method3()
-{
-    return DateTime.Now.Year;
-}
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
 
 //int year = Method3();
 //Console.WriteLine(year);
